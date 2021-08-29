6e9f5c5c8272799c6d3ac8ab90b1a2e1963b4f43
Enes Durmus <enesdurmus3738@gmail.com>
2021-08-29 16:00:54 +0300

    Home page css handled, image added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,24 @@ import "./home.scss"
 export default function Home() {
     return (
         <div className="home" id="home">
-            home
+
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/foti1.jpeg" alt="" />
+                </div>
+            </div>
+
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Enes Durmus</h1>
+                    <h3>Computer Engineering Student</h3>
+
+                    <a href="#portfolio">
+                        <img src="assets/indir (1).png" alt="" />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
