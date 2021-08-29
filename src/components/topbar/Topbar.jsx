b62763ad8b83f08325fe9e9fef15a4c370155d14
Enes Durmus <enesdurmus3738@gmail.com>
2021-08-29 14:10:51 +0300

    Hamburger menu, its animations added.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,31 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
-export default function topbar() {
+export default function topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
-                <div className="left">
-                    <a href="#home">sesaa</a>
-                </div>   
-                <div className="right">
 
+                <div className="left">
+                    <a href="#home" className="logo">sesaa</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+90 adasdasdsa</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>enesdurmus3738</span>
+                    </div>
+                </div>   
+
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
